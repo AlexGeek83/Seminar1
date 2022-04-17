6d4516a72fdccff52ae8cd9462455b788334df61
Alex <lrl.msk@yandex.ru>
2022-04-17 19:57:54 +0300

    add

---
+++ Задача4/Program.cs
@@ -0,0 +1,6 @@
+﻿//Программа которая принимает на вход 3-значное число и на выходе показывает последнюю цифру
+
+int number = 1;
+//number = number % 10
+number %= 10;
+System.Console.WriteLine(number);
