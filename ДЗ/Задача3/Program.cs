08f9ece5e9703ab7a76d3492eac342d85f3693e1
Alex <lrl.msk@yandex.ru>
2022-04-17 21:56:14 +0300

    add DZ1 zadacha 3

--- ДЗ/Задача3/Program.cs
+++ ДЗ/Задача3/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая на вход
+//принимает число и выдаёт, является ли число чётным
+//(делится ли оно на два без остатка).
+//4 -> да
+//-3 -> нет
+//7 -> нет
+
+int number;
+number = 7;
+if (number %2 == 0)
+{
+System.Console.WriteLine($"Число {number} четное");
+}
+else
+{
+    System.Console.WriteLine($"Число {number} нечетное");
+}
