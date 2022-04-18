92624eedd7a16b7293b8776946916c8a1e6d824f
Alex <lrl.msk@yandex.ru>
2022-04-18 07:53:42 +0300

    add DZ1 zadacha 4

--- ДЗ/Задача4/Program.cs
+++ ДЗ/Задача4/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая на вход
+//принимает число (N), а на выходе показывает все чётные
+//числа от 1 до N.
+//5 -> 2, 4
+//8 -> 2, 4, 6, 8
+
+int i;
+int number;
+
+i = 0;
+number = 5;
+
+while(i <= number);
+{
+if (i % 2 == 0)
+        System.Console.WriteLine(i);  
+i = i + 1;     
+}
+
