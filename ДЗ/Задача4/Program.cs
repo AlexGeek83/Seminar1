5213b428794fb4e778475203903b2579fb783265
Alex <lrl.msk@yandex.ru>
2022-04-23 10:06:56 +0300

    add дз задача4 исправил

--- ДЗ/Задача4/Program.cs
+++ ДЗ/Задача4/Program.cs
@@ -4,16 +4,27 @@
 //5 -> 2, 4
 //8 -> 2, 4, 6, 8
 
-int i;
-int number;
+//int i;
+//int number;
 
-i = 0;
-number = 5;
+//i = 0;
+//number = 5;
 
-while(i <= number);
+//while(i < number);
+//{
+//if (i % 2 == 0)
+//        System.Console.WriteLine(i);  
+//i = i + 1;     
+//}
+int N2;
+
+N2 = 5;
+
+int num = 0;
+System.Console.WriteLine($"Чётные числа из диапазона от 1 до {N2}:");
+while (num < N2)
 {
-if (i % 2 == 0)
-        System.Console.WriteLine(i);  
-i = i + 1;     
+    num++;
+    if (num % 2 == 0) System.Console.Write($"{num} ");
 }
-
+System.Console.WriteLine(); System.Console.WriteLine();
